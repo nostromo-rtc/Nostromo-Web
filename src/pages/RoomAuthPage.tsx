59d9aa59b0ad768e16e5ced6cff4005b93e43cb9
SgAkErRu <sulmpx60@yandex.ru>
2022-10-13 03:06:31 +0400

    Разметка для страницы авторизации + изменения размеров шрифтов

--- src/pages/RoomAuthPage.tsx
+++ src/pages/RoomAuthPage.tsx
@@ -1,12 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Header } from "../components/Header";
 
 import "../App.css";
+import "./RoomAuthPage.css";
 
 export const RoomAuthPage: React.FC = () =>
 {
     const { id } = useParams();
+    const [status, setStatus] = useState(true)
 
     useEffect(() =>
     {
@@ -17,7 +19,15 @@ export const RoomAuthPage: React.FC = () =>
         <div className="container flex-column">
             <Header title="Авторизация в комнате" />
             <div className="base">
-                Авторизация в {id}
+                <form id="auth" autoComplete="on" onSubmit={(ev) => {setStatus(false); ev.preventDefault()}}>
+                    {status === false ? <span className="m-a" id="status">Неправильный пароль!</span> : <></>}
+                    <span className="m-a">Вход в комнату</span>
+                    <span className="m-a" id="room-name">Тестовая</span>
+                    <input type="text" name="username" defaultValue={id} hidden />
+                    <input id="pass" type="password" name="password" placeholder="Введите пароль" />
+                    { /* TODO: это пока заглушка, нужно привязать нормальное действие */ }
+                    <input id="btn-join" type="submit" value="Войти" />
+                </form>
             </div>
         </div>
     );
