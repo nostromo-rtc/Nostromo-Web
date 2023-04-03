394625eba01589eb6c49ec2c0c07937510b7fb89
SgAkErRu <sulmpx60@yandex.ru>
2023-04-03 20:16:36 +0400

    Начата работа над кнопками в ActionPanel

--- src/components/Room/RoomActionPanel.tsx
+++ src/components/Room/RoomActionPanel.tsx
@@ -1,10 +1,30 @@
+import { Button } from "@mui/material";
 import React from 'react';
+import { BsPeopleFill } from "react-icons/bs";
+import { Tooltip } from "../Tooltip";
 
 import "./RoomActionPanel.css";
 
 export const RoomActionPanel: React.FC = () =>
-(
-    <div id="action-panel-container">
-        action-panel-container
-    </div>
-);
+{
+    const toggleUserListBtn =
+        <div className="action-btn-box">
+            <Tooltip title={"Тест"}>
+                <Button aria-label="Hide/show user list"
+                    className={
+                        "action-btn action-btn-active"
+                    }
+                    onClick={() => { console.log("1"); }}>
+                    <BsPeopleFill />
+                </Button>
+            </Tooltip>
+            <span className="action-btn-desc">Выкл</span>
+        </div>;
+
+    return (
+        <div id="action-panel-container">
+            {toggleUserListBtn}
+            {toggleUserListBtn}
+        </div>
+    );
+};
