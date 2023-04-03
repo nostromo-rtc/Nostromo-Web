92335dfb019465366831bc2c9e62672f5b128830
SgAkErRu <sulmpx60@yandex.ru>
2023-04-03 17:07:30 +0400

    Компонент для уведомлений (alert) на странице с комнатой
    Например, когда выключен звук приложения

---
+++ src/components/RoomAlert.tsx
@@ -0,0 +1,26 @@
+import "./RoomAlert.css";
+
+import { Alert, AlertColor, Collapse } from "@mui/material";
+import { ReactNode } from "react";
+
+interface RoomAlertProps
+{
+    isOpen: boolean;
+    onCloseAction: (isOpen: boolean) => void;
+    severity: AlertColor;
+    children: ReactNode;
+}
+
+export const RoomAlert: React.FC<RoomAlertProps> = ({ isOpen, onCloseAction, severity, children }) =>
+{
+    return (
+        <Collapse in={isOpen} unmountOnExit>
+            <Alert severity={severity}
+                onClose={(ev) => onCloseAction(false)}
+                className="room-alert"
+            >
+                {children}
+            </Alert>
+        </Collapse>
+    );
+};
--- src/pages/RoomPage.tsx
+++ src/pages/RoomPage.tsx
@@ -7,20 +7,55 @@ import { ActionPanel } from "../components/ActionPanel";
 
 import "./RoomPage.css";
 import { HeaderRoomToolbarProps } from "../components/HeaderRoomToolbar";
+import { RoomAlert } from "../components/RoomAlert";
+import { Link } from "@mui/material";
 
 export const RoomPage: React.FC = () =>
 {
     const roomName = "Тестовая";
+
+    const [disabledAudioAlertOpen, setDisabledAudioAlertOpen] = useState(false);
 
     useEffect(() =>
     {
         document.title = `Nostromo - Комната "${roomName}"`;
     }, []);
 
+    useEffect(() =>
+    {
+        setDisabledAudioAlertOpen(true);
+    }, []);
+
+    const disabledAudioAlertMessage =
+        <>
+            Не слышите собеседников? В данный момент у вас <b>выключен звук</b> в приложении. {"Нажмите "}
+            <Link
+                component="button"
+                variant="body2"
+                onClick={() =>
+                {
+                    setDisabledAudioAlertOpen(false);
+                }}
+            >
+                <b>здесь</b>
+            </Link>
+            , чтобы включить звук.
+        </>;
+
+    const roomAlerts =
+        <div id="room-alerts-container">
+            <RoomAlert severity="warning"
+                isOpen={disabledAudioAlertOpen}
+                onCloseAction={setDisabledAudioAlertOpen}
+                children={disabledAudioAlertMessage}
+            />
+        </div>;
+
     const videoContainer = <div id="video-container">video-container</div>;
     const chatContainer = <div id="chat-container">chat-container</div>;
     const callContainer =
         <div id="call-container">
+            {roomAlerts}
             <ActionPanel />
             {videoContainer}
         </div>;
