c6b046b3bd0321d053a7d84fdc7b459f3531a72a
SgAkErRu <sulmpx60@yandex.ru>
2023-04-05 02:27:29 +0400

    Улучшен вывод подсказок на кнопках ActionPanel

--- src/components/Room/ActionPanel/RoomActionPanel.tsx
+++ src/components/Room/ActionPanel/RoomActionPanel.tsx
@@ -72,23 +72,25 @@ export const RoomActionPanel: React.FC<RoomActionPanelProps> = ({
     };
 
     const toggleMicBtn = <>
-        <Tooltip id="tooltip-toggle-mic-btn" title={toggleMicBtnMsg} offset={10}>
-            <div className="action-btn-box non-selectable" ref={micBtnBoxRef}>
-                <Button aria-label="Start/stop capture mic"
-                    className={"action-btn " + (toggleMicBtnInfo.enabled ? "action-btn-off" : "action-btn-on")}
-                    onClick={toggleMicBtnOnClick}
-                >
-                    {toggleMicBtnInfo.enabled ? <MdMicOff /> : <MdMic />}
-                </Button>
-                <Button className="action-list-btn"
-                    onClick={toggleMicBtnMenuOpen}>
-                    <BiChevronDown />
-                </Button>
-                <span className="action-btn-desc">Микрофон</span>
-                <div className="action-btn-clickable-area non-selectable" onClick={toggleMicBtnOnClick}></div>
-                <div className="action-list-btn-clickable-area non-selectable" onClick={toggleMicBtnMenuOpen}></div>
-            </div >
-        </Tooltip>
+        <div className="action-btn-box non-selectable" ref={micBtnBoxRef}>
+            <Tooltip id="tooltip-toggle-mic-btn" title={toggleMicBtnMsg} offset={15}>
+                <div>
+                    <Button aria-label="Start/stop capture mic"
+                        className={"action-btn " + (toggleMicBtnInfo.enabled ? "action-btn-off" : "action-btn-on")}
+                        onClick={toggleMicBtnOnClick}
+                    >
+                        {toggleMicBtnInfo.enabled ? <MdMicOff /> : <MdMic />}
+                    </Button>
+                    <div className="action-btn-clickable-area non-selectable" onClick={toggleMicBtnOnClick}></div>
+                </div>
+            </Tooltip>
+            <Button className="action-list-btn"
+                onClick={toggleMicBtnMenuOpen}>
+                <BiChevronDown />
+            </Button>
+            <span className="action-btn-desc">Микрофон</span>
+            <div className="action-list-btn-clickable-area non-selectable" onClick={toggleMicBtnMenuOpen}></div>
+        </div>
         <MicBtnMenu
             anchorRef={micBtnBoxRef}
             open={toggleMicBtnInfo.menuOpen}
@@ -124,19 +126,21 @@ export const RoomActionPanel: React.FC<RoomActionPanelProps> = ({
     };
 
     const toggleCamBtn =
-        <Tooltip title={toggleCamBtnMsg} offset={10}>
-            <div className="action-btn-box non-selectable">
-                <Button aria-label="Start/stop capture webcam"
-                    className={"action-btn " + (toggleCamBtnInfo.enabled ? "action-btn-off" : "action-btn-on")}
-                    onClick={toggleCamBtnOnClick}>
-                    {toggleCamBtnInfo.enabled ? <MdVideocamOff /> : <MdVideocam />}
-                </Button>
-                <Button className="action-list-btn"><BiChevronDown /></Button>
-                <span className="action-btn-desc">Камера</span>
-                <div className="action-btn-clickable-area non-selectable" onClick={toggleCamBtnOnClick}></div>
-                <div className="action-list-btn-clickable-area non-selectable" onClick={() => { console.log("2"); }}></div>
-            </div >
-        </Tooltip>;
+        <div className="action-btn-box non-selectable">
+            <Tooltip title={toggleCamBtnMsg} offset={10}>
+                <div>
+                    <Button aria-label="Start/stop capture webcam"
+                        className={"action-btn " + (toggleCamBtnInfo.enabled ? "action-btn-off" : "action-btn-on")}
+                        onClick={toggleCamBtnOnClick}>
+                        {toggleCamBtnInfo.enabled ? <MdVideocamOff /> : <MdVideocam />}
+                    </Button>
+                    <div className="action-btn-clickable-area non-selectable" onClick={toggleCamBtnOnClick}></div>
+                </div>
+            </Tooltip>
+            <Button className="action-list-btn"><BiChevronDown /></Button>
+            <span className="action-btn-desc">Камера</span>
+            <div className="action-list-btn-clickable-area non-selectable" onClick={() => { console.log("2"); }}></div>
+        </div>;
 
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
@@ -148,19 +152,22 @@ export const RoomActionPanel: React.FC<RoomActionPanelProps> = ({
     };
 
     const toggleScreenBtn =
-        <Tooltip id="tooltip-toggle-screen-btn" title={toggleScreenBtnMsg} offset={10}>
-            <div className="action-btn-box non-selectable">
-                <Button aria-label="Start/stop capture webcam"
-                    className={"action-btn " + (toggleScreenBtnInfo.enabled ? "action-btn-off" : "action-btn-on")}
-                    onClick={toggleScreenBtnOnClick}>
-                    {toggleScreenBtnInfo.enabled ? <MdStopScreenShare /> : <MdScreenShare />}
-                </Button>
-                <Button className="action-list-btn"><BiChevronDown /></Button>
-                <span className="action-btn-desc">Экран</span>
-                <div className="action-btn-clickable-area non-selectable" onClick={toggleScreenBtnOnClick}></div>
-                <div className="action-list-btn-clickable-area non-selectable" onClick={() => { console.log("2"); }}></div>
-            </div >
-        </Tooltip>;
+
+        <div className="action-btn-box non-selectable">
+            <Tooltip id="tooltip-toggle-screen-btn" title={toggleScreenBtnMsg} offset={10}>
+                <div>
+                    <Button aria-label="Start/stop capture webcam"
+                        className={"action-btn " + (toggleScreenBtnInfo.enabled ? "action-btn-off" : "action-btn-on")}
+                        onClick={toggleScreenBtnOnClick}>
+                        {toggleScreenBtnInfo.enabled ? <MdStopScreenShare /> : <MdScreenShare />}
+                    </Button>
+                    <div className="action-btn-clickable-area non-selectable" onClick={toggleScreenBtnOnClick}></div>
+                </div>
+            </Tooltip>
+            <Button className="action-list-btn"><BiChevronDown /></Button>
+            <span className="action-btn-desc">Экран</span>
+            <div className="action-list-btn-clickable-area non-selectable" onClick={() => { console.log("2"); }}></div>
+        </div>;
 
 
     return (
