889be0695113a03f8e86c843d00e9f67c2689608
SgAkErRu <sulmpx60@yandex.ru>
2023-07-17 07:14:13 +0400

    Небольшой рефакторинг в UserList

--- src/components/Room/UserList.tsx
+++ src/components/Room/UserList.tsx
@@ -20,9 +20,6 @@ export const UserList: React.FC<UserListProps> = ({
     transitionDuration
 }) =>
 {
-    const INDEX_OF_FIRST_SYMBOL = 0;
-    const EMPTY_LIST_LENGTH = 0;
-
     const [onlineUserList, setOnlineUserList] = useState<UserInfo[]>([]);
     const [offlineUserList, setOfflineUserList] = useState<UserInfo[]>([]);
 
@@ -30,120 +27,6 @@ export const UserList: React.FC<UserListProps> = ({
     {
         // TODO: Сделать переключение на стрелки.
         console.log(ev.key);
-    };
-
-    interface UserListItemProps
-    {
-        user: UserInfo;
-    }
-
-    const UserListItem: React.FC<UserListItemProps> = ({ user }) =>
-    {
-        const DEFAULT_VOLUME_VALUE = 100;
-
-        const [menuPosition, setMenuPosition] = useState<AnchorPosition | null>(null);
-        const [open, setOpen] = useState<boolean>(false);
-        const [userMuted, setUserMuted] = useState<boolean>(false);
-        const [volume, setVolume] = useState<number>(DEFAULT_VOLUME_VALUE);
-
-        const handleClose = (): void =>
-        {
-            setOpen(false);
-            setMenuPosition(null);
-        };
-
-        const handleContextMenu: DivClickEventHandler = (ev) =>
-        {
-            ev.preventDefault();
-            setMenuPosition({ left: ev.clientX, top: ev.clientY });
-            setOpen(true);
-        };
-
-        return (<>
-            {/* aria-expanded является допустимым свойствои для role=listitem */}
-            {/* eslint-disable-next-line jsx-a11y/role-supports-aria-props */}
-            <div className="user-list-item non-selectable"
-                tabIndex={-1}
-                role="listitem"
-                aria-expanded={open}
-                onContextMenu={handleContextMenu}
-            >
-                <Avatar className="user-list-item-avatar" children={user.name[INDEX_OF_FIRST_SYMBOL]} />
-                <div className="user-list-item-info">
-                    <span className="user-list-item-info-name">{user.name}</span>
-                    <span className="user-list-item-info-id">#{user.id}</span>
-                </div>
-            </div>
-            <Menu
-                anchorPosition={menuPosition ?? undefined}
-                open={open}
-                onClose={handleClose}
-                transitionDuration={transitionDuration}
-                popperPlacement="left-start"
-            >
-                <MenuList open={open} disableAutoFocusItem>
-                    <MenuItemCheckbox
-                        text="Приглушить звук"
-                        isChecked={userMuted}
-                        onClick={getToggleFunc(setUserMuted)}
-                    />
-                    <Divider className="menu-divider" />
-                    <MenuItemSlider
-                        text="Громкость звука"
-                        value={volume}
-                        setValue={setVolume}
-                    />
-                    <Divider className="menu-divider" />
-                    <MenuItemWithIcon
-                        text="Скопировать имя пользователя"
-                        icon={<HiIdentification />}
-                        endIcon
-                        onClick={async () =>
-                        {
-                            await navigator.clipboard.writeText(user.name);
-                            handleClose();
-                        }}
-                    />
-                    <MenuItemWithIcon
-                        text="Скопировать ID пользователя"
-                        icon={<HiHashtag />}
-                        endIcon
-                        onClick={async () =>
-                        {
-                            await navigator.clipboard.writeText(user.id);
-                            handleClose();
-                        }}
-                    />
-                </MenuList>
-            </Menu>
-        </>
-        );
-    };
-
-    const userListToListItems = (user: UserInfo, index: number): JSX.Element =>
-    {
-        return (
-            <UserListItem
-                key={user.id}
-                user={user}
-            />
-        );
-    };
-
-    interface UserListSectionProps
-    {
-        sectionLabel: string;
-        list: UserInfo[];
-    }
-
-    const UserListSection: React.FC<UserListSectionProps> = ({ sectionLabel, list }) =>
-    {
-        const content = <>
-            <MenuSectionLabel text={`${sectionLabel} — ${list.length}`} />
-            {list.map(userListToListItems)}
-        </>;
-
-        return ((list.length > EMPTY_LIST_LENGTH) ? content : <></>);
     };
 
     useEffect(() =>
@@ -162,8 +45,128 @@ export const UserList: React.FC<UserListProps> = ({
             onKeyDown={handleListKeyDown}
             role="list"
         >
-            <UserListSection sectionLabel="В сети" list={onlineUserList} />
-            <UserListSection sectionLabel="Не в сети" list={offlineUserList} />
+            <UserListSection sectionLabel="В сети" list={onlineUserList} transitionDuration={transitionDuration} />
+            <UserListSection sectionLabel="Не в сети" list={offlineUserList} transitionDuration={transitionDuration} />
         </div>
+    );
+};
+
+interface UserListSectionProps
+{
+    sectionLabel: string;
+    list: UserInfo[];
+    transitionDuration: number;
+}
+
+const UserListSection: React.FC<UserListSectionProps> = ({ sectionLabel, list, transitionDuration }) =>
+{
+    const EMPTY_LIST_LENGTH = 0;
+
+    const userListToListItems = (user: UserInfo, index: number): JSX.Element =>
+    {
+        return (
+            <UserListItem
+                key={user.id}
+                user={user}
+                transitionDuration={transitionDuration}
+            />
+        );
+    };
+
+    const content = <>
+        <MenuSectionLabel text={`${sectionLabel} — ${list.length}`} />
+        {list.map(userListToListItems)}
+    </>;
+
+    return ((list.length > EMPTY_LIST_LENGTH) ? content : <></>);
+};
+
+interface UserListItemProps
+{
+    user: UserInfo;
+    transitionDuration: number;
+}
+
+const UserListItem: React.FC<UserListItemProps> = ({ user, transitionDuration }) =>
+{
+    const INDEX_OF_FIRST_SYMBOL = 0;
+    const DEFAULT_VOLUME_VALUE = 100;
+
+    const [menuPosition, setMenuPosition] = useState<AnchorPosition | null>(null);
+    const [open, setOpen] = useState<boolean>(false);
+    const [userMuted, setUserMuted] = useState<boolean>(false);
+    const [volume, setVolume] = useState<number>(DEFAULT_VOLUME_VALUE);
+
+    const handleClose = (): void =>
+    {
+        setOpen(false);
+        setMenuPosition(null);
+    };
+
+    const handleContextMenu: DivClickEventHandler = (ev) =>
+    {
+        ev.preventDefault();
+        setMenuPosition({ left: ev.clientX, top: ev.clientY });
+        setOpen(true);
+    };
+
+    return (<>
+        {/* aria-expanded является допустимым свойствои для role=listitem */}
+        {/* eslint-disable-next-line jsx-a11y/role-supports-aria-props */}
+        <div className="user-list-item non-selectable"
+            tabIndex={-1}
+            role="listitem"
+            aria-expanded={open}
+            onContextMenu={handleContextMenu}
+        >
+            <Avatar className="user-list-item-avatar" children={user.name[INDEX_OF_FIRST_SYMBOL]} />
+            <div className="user-list-item-info">
+                <span className="user-list-item-info-name">{user.name}</span>
+                <span className="user-list-item-info-id">#{user.id}</span>
+            </div>
+        </div>
+        <Menu
+            anchorPosition={menuPosition ?? undefined}
+            open={open}
+            onClose={handleClose}
+            transitionDuration={transitionDuration}
+            popperPlacement="left-start"
+        >
+            <MenuList open={open} disableAutoFocusItem>
+                <MenuItemCheckbox
+                    text="Приглушить звук"
+                    isChecked={userMuted}
+                    onClick={getToggleFunc(setUserMuted)}
+                />
+                <Divider className="menu-divider" />
+                <MenuItemSlider
+                    text="Громкость звука"
+                    value={volume}
+                    setValue={setVolume}
+                />
+                <Divider className="menu-divider" />
+                <MenuItemWithIcon
+                    text="Скопировать имя пользователя"
+                    icon={<HiIdentification />}
+                    endIcon
+                    onClick={async () =>
+                    {
+                        await navigator.clipboard.writeText(user.name);
+                        handleClose();
+                    }}
+                />
+                <MenuItemWithIcon
+                    text="Скопировать ID пользователя"
+                    icon={<HiHashtag />}
+                    endIcon
+                    onClick={async () =>
+                    {
+                        await navigator.clipboard.writeText(user.id);
+                        handleClose();
+                    }}
+                />
+            </MenuList>
+        </Menu>
+    </>
     );
 };
