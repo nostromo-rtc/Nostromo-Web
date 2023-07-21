e929b28def751af4055e9af7da29e0833968a856
SgAkErRu <sulmpx60@yandex.ru>
2023-07-21 13:54:08 +0400

    Название файла в карточке теперь имеет ограничение в три строки (очередь файлов)

--- src/components/Room/RoomChat/FileLoadingCard.tsx
+++ src/components/Room/RoomChat/FileLoadingCard.tsx
@@ -25,7 +25,11 @@ export const FileLoadingCard: React.FC<FileProps> = (props) =>
             <div className='remove-file-btn'
                 onClick={() => { if (props.onRemove) props.onRemove(props.file.fileId); }}>Х</div>
             <div className='file-cards-icon'><FcFile className='file-icon' /></div>
-            <div className='file-cards-desc'>{props.file.name}</div>
+            <div className='file-cards-desc-wrapper'>
+                <div className="vertical-expander" />
+                <div className='file-cards-desc' title={props.file.name}>{props.file.name}</div>
+                <div className="vertical-expander" />
+            </div>
             <div className="file-cards-progress">
                 <progress id="progressBar" value={props.progress} max={props.file.size}></progress>
                 <div className="progress-load">{(props.progress / MB_VAL).toFixed(FILE_SIZE_PRESCISSION)}MB из {(props.file.size / MB_VAL).toFixed(FILE_SIZE_PRESCISSION)}MB</div>
