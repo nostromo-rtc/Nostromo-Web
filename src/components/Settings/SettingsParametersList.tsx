cc779cd2103978b1981ae95c922b02934fa1c409
Vladislav Tarakanov <vladislav.tarakanov@bk.ru>
2023-08-07 15:46:16 +0400

    Изменение внешнего вида SettingsParametersList
    - Функция getParametersList разделена на 4 части: loadSelectedCategory,
    loadSection, loadGroup и loadParameter
    - Изменены размеры текста для заголовков категорий, секций и групп
    - Изменены отступы сверху и снизу для категорий, секций и групп
    - У первых секций и групп в блоке уменьшен отступ, у следующих увеличен

--- src/components/Settings/SettingsParametersList.tsx
+++ src/components/Settings/SettingsParametersList.tsx
@@ -1,6 +1,6 @@
 import { FC, useContext } from "react";
 import "./SettingsParametersList.css";
-import { ParametersInfoMap, Settings, useSettings } from "../../services/SettingsService";
+import { Category, Group, ParametersInfoMap, Section, Settings, useSettings } from "../../services/SettingsService";
 import { ListItemInput, ListItemSelect, ListItemSlider, ListItemSwitch } from "../Base/List/ListItems";
 import { List } from "../Base/List/List";
 import { SettingsContext } from "../../App";
@@ -59,7 +59,137 @@ export const SettingsParametersList: FC<SettingsParametersListProps> = ({ parame
         });
     };
 
-    const getParametersList = (): JSX.Element[] =>
+    const loadParameter =
+    (
+        elements: JSX.Element[],
+        groupMap: Group,
+        category: string,
+        section: string,
+        group: string,
+        parameter: string
+    ): void =>
+    {
+        // TODO: аналогично с помощью такого `id` можно вытаскивать информацию
+        // для рендера и других элементов (категории, секции, группы),
+        // при наличии соответствующих объектов с информацией как parametersInfoMap 
+        // для всех этих видов элементов.
+        const parameterId = `${category}.${section}.${group}.${parameter}`;
+        const paramValue = parametersInfoMap[parameterId];
+
+        if (paramValue.type === "Switch")
+        {
+            elements.push(
+                <ListItemSwitch
+                    key={parameterId}
+                    description={parametersInfoMap[parameterId].description}
+                    text={parametersInfoMap[parameterId].name}
+                    checked={groupMap[parameter] as boolean}
+                    setChecked={(val) =>
+                    {
+                        handleSwitch(section, group, parameter, val);
+                    }}
+                />
+            );
+        }
+        else if (paramValue.type === "Slider")
+        {
+            elements.push(
+                <ListItemSlider
+                    key={parameterId}
+                    className="list-item"
+                    text={parametersInfoMap[parameterId].name + ": " + groupMap[parameter].toString()}
+                    value={Number(groupMap[parameter])}
+                    setValue={(val) =>
+                    {
+                        handleSlider(section, group, parameter, val);
+                    }}
+                />
+            );
+        }
+        else if (paramValue.type === "Input")
+        {
+            elements.push(
+                <ListItemInput
+                    key={parameterId}
+                    description={parametersInfoMap[parameterId].description}
+                    text={parametersInfoMap[parameterId].name}
+                    value={groupMap[parameter] as string}
+                    setValue={(val) =>
+                    {
+                        handleInput(section, group, parameter, val.toString());
+                    }}
+                />
+            );
+        }
+        else if (paramValue.type as string === "Select")  // as string для проверки на получение недопустимого типа из хранилища
+        {
+            const selectList: string[] = ["First", "Second", "Third", "Fourth", "Fifth", "Sixth"];
+            elements.push(
+                <ListItemSelect
+                    key={parameterId}
+                    list={selectList}
+                    value={groupMap[parameter] as string}
+                    setValue={(val) =>
+                    {
+                        handleSelect(section, group, parameter, val.toString());
+                    }}
+                    description={parametersInfoMap[parameterId].description}
+                    text={parametersInfoMap[parameterId].name}
+                />
+            );
+        }
+        else
+        {
+            elements.push(
+                <div key={parameterId}>
+                    {parametersInfoMap[parameterId].name}
+                </div>
+            );
+        }
+    };
+
+    const loadGroup =
+    (
+        elements: JSX.Element[],
+        sectionMap: Section,
+        category: string,
+        section: string,
+        group: string
+    ): void =>
+    {
+        const groupId = `${category}.${section}.${group}`;
+        elements.push(
+            <p className="settings-group-label" key={groupId}>{group}</p>
+        );
+
+        const groupMap = sectionMap[group];
+        for (const parameter in groupMap)
+        {
+            loadParameter(elements, groupMap, category, section, group, parameter);
+        }
+    };
+
+    const loadSection =
+    (
+        elements: JSX.Element[],
+        categoryMap: Category,
+        category: string,
+        section: string
+    ): void =>
+    {
+        const sectionId = `${category}.${section}`;
+        elements.push(
+            <p className="settings-section-label" key={sectionId}>{section}</p>
+        );
+
+        const sectionMap = categoryMap[section];
+        for (const group in sectionMap)
+        {
+            loadGroup(elements, sectionMap, category, section, group);
+        }
+    };
+
+    const loadSelectedCategory = (): JSX.Element[] =>
     {
         const parametersList: JSX.Element[] = [
             <p className="settings-category-label" key={selectedCategory}>{selectedCategory}</p>
@@ -68,106 +198,13 @@ export const SettingsParametersList: FC<SettingsParametersListProps> = ({ parame
         const categoryMap = settings[selectedCategory];
         for (const section in categoryMap)
         {
-            const sectionId = `${selectedCategory}.${section}`;
-            parametersList.push(
-                <p className="settings-section-label" key={sectionId}>{section}</p>
-            );
-
-            const sectionMap = categoryMap[section];
-            for (const group in sectionMap)
-            {
-                const groupId = `${selectedCategory}.${section}.${group}`;
-                parametersList.push(
-                    <p className="settings-group-label" key={groupId}>{group}</p>
-                );
-
-                const groupMap = sectionMap[group];
-                for (const parameter in groupMap)
-                {
-                    // TODO: аналогично с помощью такого `id` можно вытаскивать информацию
-                    // для рендера и других элементов (категории, секции, группы),
-                    // при наличии соответствующих объектов с информацией как parametersInfoMap 
-                    // для всех этих видов элементов.
-                    const parameterId = `${selectedCategory}.${section}.${group}.${parameter}`;
-
-                    if (parametersInfoMap[parameterId].type === "Switch")
-                    {
-                        parametersList.push(
-                            <ListItemSwitch
-                                key={parameterId}
-                                description={parametersInfoMap[parameterId].description}
-                                text={parametersInfoMap[parameterId].name}
-                                checked={groupMap[parameter] as boolean}
-                                setChecked={(val) =>
-                                {
-                                    handleSwitch(section, group, parameter, val);
-                                }}
-                            />
-                        );
-                    }
-                    else if (parametersInfoMap[parameterId].type === "Slider")
-                    {
-                        parametersList.push(
-                            <ListItemSlider
-                                key={parameterId}
-                                className="list-item"
-                                text={parametersInfoMap[parameterId].name + ": " + groupMap[parameter].toString()}
-                                value={Number(groupMap[parameter])}
-                                setValue={(val) =>
-                                {
-                                    handleSlider(section, group, parameter, val);
-                                }}
-                            />
-                        );
-                    }
-                    else if (parametersInfoMap[parameterId].type === "Input")
-                    {
-                        parametersList.push(
-                            <ListItemInput
-                                key={parameterId}
-                                description={parametersInfoMap[parameterId].description}
-                                text={parametersInfoMap[parameterId].name}
-                                value={groupMap[parameter] as string}
-                                setValue={(val) =>
-                                {
-                                    handleInput(section, group, parameter, val.toString());
-                                }}
-                            />
-                        );
-                    }
-                    else if (parametersInfoMap[parameterId].type === "Select")
-                    {
-                        const selectList: string[] = ["First", "Second", "Third", "Fourth", "Fifth", "Sixth"];
-                        parametersList.push(
-                            <ListItemSelect
-                                key={parameterId}
-                                list={selectList}
-                                value={groupMap[parameter] as string}
-                                setValue={(val) =>
-                                {
-                                    handleSelect(section, group, parameter, val.toString());
-                                }}
-                                description={parametersInfoMap[parameterId].description}
-                                text={parametersInfoMap[parameterId].name}
-                            />
-                        );
-                    }
-                    else
-                    {
-                        parametersList.push(
-                            <div key={parameterId}>
-                                {parametersInfoMap[parameterId].name}
-                            </div>
-                        );
-                    }
-                }
-            }
+            loadSection(parametersList, categoryMap, selectedCategory, section);
         }
 
         return parametersList;
     };
 
     return (
-        <List>{getParametersList()}</List>
+        <List>{loadSelectedCategory()}</List>
     );
 };
