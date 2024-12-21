1003b3eac7cba702ef2705a63d9bcf3168b23a95
SgAkErRu <sulmpx60@yandex.ru>
2024-12-21 08:16:12 +0400

    Add DisplayStateNotificationsService

---
+++ src/services/Notifications/DisplayStateNotificationsService.ts
@@ -0,0 +1,64 @@
+import { NotificationsService, NotificationSeverity } from "../NotificationsService";
+import { DisplayState } from "../UserMediaService/DisplayStateModel";
+import { UserMediaService } from "../UserMediaService/UserMediaService";
+
+export class DisplayStateNotificationsService
+{
+    private readonly m_userMediaService: UserMediaService;
+    private readonly m_notificationService: NotificationsService;
+    private m_prevDisplayState: DisplayState | null = null;
+
+    // TODO: add SoundAlertService
+    public constructor(
+        userMediaService: UserMediaService,
+        notificationsService: NotificationsService
+    )
+    {
+        this.m_notificationService = notificationsService;
+        this.m_userMediaService = userMediaService;
+
+        this.subscribeToStateModel();
+    }
+
+    private subscribeToStateModel(): void
+    {
+        this.m_userMediaService.displayStateModel.subscribe(() => { this.handleChangeState(); });
+    }
+
+    private handleChangeState(): void
+    {
+        const displayState = this.m_userMediaService.displayStateModel.getStateSnapshot();
+
+        if (displayState === DisplayState.IDLE
+            && this.m_prevDisplayState === DisplayState.LOADING)
+        {
+            this.handleFailedGetDisplay();
+        }
+        else if (displayState === DisplayState.IDLE
+            && this.m_prevDisplayState !== DisplayState.LOADING
+        )
+        {
+            this.handleDisableDisplay();
+        }
+
+        this.m_prevDisplayState = displayState;
+    }
+
+    private handleDisableDisplay(): void
+    {
+        this.m_notificationService.add({
+            label: `Демонстрация экрана`,
+            description: `Демонстрация экрана завершена.`,
+            severity: NotificationSeverity.WARNING
+        });
+    }
+
+    private handleFailedGetDisplay(): void
+    {
+        this.m_notificationService.add({
+            label: `Ошибка при захвате экрана`,
+            description: `Экран захватить не удалось.\nПроверьте разрешения браузера на доступ к экрану.`,
+            severity: NotificationSeverity.ERROR
+        });
+    }
+}
