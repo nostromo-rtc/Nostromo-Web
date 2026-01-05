4b093e8df8e3b0048bc2cb67af8a10ed52a76efc
SgAkErRu <sulmpx60@yandex.ru>
2026-01-05 19:40:10 +0400

    Settings: simplify settings and add GeneralSettings page
    - Remove rendering page from settings description object
    
    - Remove settings description object
    
    - Add ISettings interface and DefaultSettings object
    
    - Create page for GeneralSettings

--- src/components/Settings/SettingsParametersList.tsx
+++ src/components/Settings/SettingsParametersList.tsx
@@ -1,88 +1,34 @@
 /*
-    SPDX-FileCopyrightText: 2023 Vladislav Tarakanov <vladislav.tarakanov@bk.ru>
-    SPDX-FileCopyrightText: 2023 Sergey Katunin <sulmpx60@yandex.ru>
-    SPDX-FileCopyrightText: 2023 Amin Irgaliev <irgaliev01@mail.ru>
+    SPDX-FileCopyrightText: 2023-2026 Sergey Katunin <sulmpx60@yandex.ru>
 
     SPDX-License-Identifier: BSD-2-Clause
 */
 
-import { FC, useContext, useRef, useState } from "react";
-import { SettingsContext } from "../../AppWrapper";
-import { Category, Group, ParameterValue, ParametersInfoMap, Section, Settings, useSettings } from "../../services/SettingsService";
-import { List } from "../Base/List/List";
-import { ListItemButton, ListItemInput, ListItemSelect, ListItemSlider, ListItemSwitch } from "../Base/List/ListItems";
-import { RestoreSettingsDialog } from "./RestoreSettingsDialog";
+import React from "react";
+
+import { ISettingsCategories } from "../../pages/SettingsLayer";
+import { GeneralSettings } from "./SettingsCategories/GeneralSettings";
+
 import "./SettingsParametersList.css";
 
 interface SettingsParametersListProps
 {
-    parametersInfoMap: ParametersInfoMap;
-    selectedCategory: string;
+    selectedCategoryId: string;
+    categories: ISettingsCategories;
 }
 
-export const SettingsParametersList: FC<SettingsParametersListProps> = ({ parametersInfoMap, selectedCategory }) =>
+export interface SettingsCategoryProps
 {
-    const settingsService = useContext(SettingsContext);
-    const settings = useSettings(settingsService);
+    categoryName: string;
+}
 
-    /* -- Для диалогового окна подтверждения сброса настроек. -- */
-
-    // Для выставления фокуса обратно на кнопку сброса
-    // после закрытия диалогового окна со сбросом настроек.
-    const restoreSettingsBtnRef = useRef<HTMLButtonElement>(null);
-
-    const [showRestoreSettingsDialog, setShowRestoreSettingsDialog] = useState<boolean>(false);
-
-    const closeRestoreSettingsDialog = (): void =>
-    {
-        setShowRestoreSettingsDialog(false);
-        restoreSettingsBtnRef.current?.focus();
-    };
-
-    const handleRestoreSettingsConfirm = (): void =>
-    {
-        settingsService.restoreToDefault();
-        closeRestoreSettingsDialog();
-    };
-
-    /* --------------------------------------------------------- */
-
-    const handleParameterChange = (section: string, group: string, param: string, val: ParameterValue): void =>
-    {
-        settingsService.setSettings((prev: Settings) =>
-        {
-            prev[selectedCategory][section][group][param] = val;
-        });
-    };
-
-    const loadParameter = (
-        elements: JSX.Element[],
-        groupMap: Group,
-        category: string,
-        section: string,
-        group: string,
-        parameter: string
-    ): void =>
-    {
-        const parameterId = `${category}.${section}.${group}.${parameter}`;
-        const paramValue = parametersInfoMap[parameterId];
-
-        if (paramValue.type === "Switch")
-        {
-            elements.push(
-                <ListItemSwitch
-                    key={parameterId}
-                    label={parametersInfoMap[parameterId].name}
-                    description={parametersInfoMap[parameterId].description}
-                    value={groupMap[parameter] as boolean}
-                    onValueChange={(val) =>
-                    {
-                        handleParameterChange(section, group, parameter, val);
-                    }}
-                />
-            );
-        }
-        else if (paramValue.type === "Slider")
+export const SettingsParametersList: React.FC<SettingsParametersListProps> = ({
+    selectedCategoryId,
+    categories
+}) =>
+{
+    /*
+        if (paramValue.type === "Slider")
         {
             elements.push(
                 <ListItemSlider
@@ -129,96 +75,21 @@ export const SettingsParametersList: FC<SettingsParametersListProps> = ({ parame
                 />
             );
         }
-        else
-        {
-            elements.push(
-                <div key={parameterId}>
-                    {parametersInfoMap[parameterId].name}
-                </div>
-            );
-        }
-    };
+    };*/
 
-    const loadGroup = (
-        elements: JSX.Element[],
-        sectionMap: Section,
-        category: string,
-        section: string,
-        group: string
-    ): void =>
+    // <p className="settings-group-label" key={groupId}>{group}</p>
+
+    switch (selectedCategoryId)
     {
-        const groupId = `${category}.${section}.${group}`;
-        elements.push(
-            <p className="settings-group-label" key={groupId}>{group}</p>
-        );
-
-        const groupMap = sectionMap[group];
-        for (const parameter in groupMap)
-        {
-            loadParameter(elements, groupMap, category, section, group, parameter);
-        }
-    };
-
-    const loadSection = (
-        elements: JSX.Element[],
-        categoryMap: Category,
-        category: string,
-        section: string
-    ): void =>
-    {
-        const sectionId = `${category}.${section}`;
-        elements.push(
-            <p className="settings-section-label" key={sectionId}>{section}</p>
-        );
-
-        const sectionMap = categoryMap[section];
-        for (const group in sectionMap)
-        {
-            loadGroup(elements, sectionMap, category, section, group);
-        }
-    };
-
-    const loadSelectedCategory = (): JSX.Element[] =>
-    {
-        const parametersList: JSX.Element[] = [
-            <p className="settings-category-label" key={selectedCategory}>{selectedCategory}</p>
-        ];
-
-        const categoryMap = settings[selectedCategory];
-        for (const section in categoryMap)
-        {
-            loadSection(parametersList, categoryMap, selectedCategory, section);
-        }
-
-        // Добавим кнопку для сброса настроек в конец
-        // для категории "general".
-        if (selectedCategory === "general")
-        {
-            parametersList.push(
-                <ListItemButton
-                    key={"restore-settings-btn"}
-                    btnRef={restoreSettingsBtnRef}
-                    label={"Сбросить все настройки до стандартных"}
-                    btnLabel={"Сбросить найстроки"}
-                    onBtnClick={() => { setShowRestoreSettingsDialog(true); }}
-                />
-            );
-        }
-
-        return parametersList;
-    };
-
-    const restoreSettingsDialog = (
-        <RestoreSettingsDialog
-            onConfirm={handleRestoreSettingsConfirm}
-            onCancel={closeRestoreSettingsDialog}
-        />
-    );
-
-    return (
-        <>
-            {showRestoreSettingsDialog ? restoreSettingsDialog : <></>}
-            <List id="settings-parameters-list">{loadSelectedCategory()}</List>
-        </>
-    );
+        case categories.general.id:
+            return <GeneralSettings categoryName={categories.general.name} />;
+        case categories.audio.id:
+            return <></>;
+        case categories.video.id:
+            return <></>;
+        case categories.display.id:
+            return <></>;
+        default:
+            return <></>;
+    }
 };
---
+++ src/services/Settings/SettingsService.ts
@@ -0,0 +1,87 @@
+/*
+    SPDX-FileCopyrightText: 2023 Sergey Katunin <sulmpx60@yandex.ru>
+    SPDX-FileCopyrightText: 2023 Vladislav Tarakanov <vladislav.tarakanov@bk.ru>
+
+    SPDX-License-Identifier: BSD-2-Clause
+*/
+
+import { useSyncExternalStore } from "react";
+
+import { AbstractExternalStorage } from "../../utils/AbstractExternalStorage";
+import { cloneObject, overrideValues } from "../../utils/Utils";
+
+import { ISettings, DefaultSettings } from "./Settings";
+
+/* eslint-disable @typescript-eslint/naming-convention */
+export type ParameterType = "Input" | "Select" | "Slider" | "Switch" | "Unknown";
+export type ParameterValue = boolean | number | string;
+export const LOCAL_STORAGE_SETTINGS = "nostromo-settings";
+
+type SettingsSetCallback = (prev: ISettings) => void;
+
+export class SettingService extends AbstractExternalStorage
+{
+    private currentSettings: ISettings = DefaultSettings;
+    private shapshot: ISettings = DefaultSettings;
+
+    public constructor()
+    {
+        super();
+
+        this.currentSettings = cloneObject(DefaultSettings);
+        const storedSettingsJson = localStorage.getItem(LOCAL_STORAGE_SETTINGS);
+        if (storedSettingsJson === null)
+        {
+            this.restoreToDefault();
+            return;
+        }
+
+        try
+        {
+            const storedSettings = JSON.parse(storedSettingsJson) as ISettings;
+            overrideValues(this.currentSettings, storedSettings);
+
+            localStorage.setItem(LOCAL_STORAGE_SETTINGS, JSON.stringify(this.currentSettings));
+        }
+        catch
+        {
+            this.restoreToDefault();
+            return;
+        }
+        this.saveSnapshot();
+    }
+
+    public restoreToDefault(): void
+    {
+        this.currentSettings = cloneObject(DefaultSettings);
+        this.saveSnapshot();
+        localStorage.setItem(LOCAL_STORAGE_SETTINGS, JSON.stringify(this.currentSettings));
+        this.notifyListeners();
+    }
+
+    public setSettings(callback: SettingsSetCallback): void
+    {
+        callback(this.currentSettings);
+        this.saveSnapshot();
+        localStorage.setItem(LOCAL_STORAGE_SETTINGS, JSON.stringify(this.currentSettings));
+        this.notifyListeners();
+    }
+
+    public getSettingsSnapshot(): ISettings
+    {
+        return this.shapshot;
+    }
+
+    private saveSnapshot(): void
+    {
+        this.shapshot = cloneObject(this.currentSettings);
+    }
+}
+
+export function useSettings(service: SettingService): ISettings
+{
+    return useSyncExternalStore(
+        (listener: () => void) => service.subscribe(listener),
+        () => service.getSettingsSnapshot()
+    );
+}
