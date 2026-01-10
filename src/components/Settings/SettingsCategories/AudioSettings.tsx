736241caae6fac702655f96682efb78df7518fab
SgAkErRu <sulmpx60@yandex.ru>
2026-01-10 13:54:35 +0400

    AudioSettings: add feature to select and capture mic in settings

--- src/components/Settings/SettingsCategories/AudioSettings.tsx
+++ src/components/Settings/SettingsCategories/AudioSettings.tsx
@@ -4,15 +4,18 @@
     SPDX-License-Identifier: BSD-2-Clause
 */
 
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 
 import { SettingsContext, UserMediaServiceContext } from "../../../AppWrapper";
 import { ISettings } from "../../../services/Settings/Settings";
 import { useSettings } from "../../../services/Settings/SettingsService";
 import { useAudioVolumeStorage } from "../../../services/UserMediaService/AudioVolumeStorage";
+import { MicState, useMicStateModel } from "../../../services/UserMediaService/MicStateModel";
+import { useUserMediaDeviceStorage } from "../../../services/UserMediaService/UserMediaDeviceStorage";
 import { useUserMediaStreamStorage } from "../../../services/UserMediaService/UserMediaStreamStorage";
+import { NumericConstants as NC } from "../../../utils/NumericConstants";
 import { List } from "../../Base/List/List";
-import { ListItemSlider, ListItemSwitch } from "../../Base/List/ListItems";
+import { ListItemButton, ListItemSelect, ListItemSelectOption, ListItemSlider, ListItemSwitch } from "../../Base/List/ListItems";
 import { SettingsCategoryProps } from "../SettingsParametersList";
 
 const ZERO_VOLUME = 0;
@@ -23,16 +26,58 @@ export const AudioSettings: React.FC<SettingsCategoryProps> = ({ categoryName })
     const settings = useSettings(settingsService);
 
     const userMediaService = useContext(UserMediaServiceContext);
+    const micStateInfo = useMicStateModel(userMediaService.micStateModel);
+
     const streams = useUserMediaStreamStorage(userMediaService.streamStorage);
     const audioVolumeStorage = useAudioVolumeStorage(userMediaService.audioVolumeStorage);
 
     const micStream = streams.find((s) => s.type === "mic");
     const micVolume = audioVolumeStorage.find((i) => i.streamId === micStream?.stream.id);
 
+    const mediaDevices = useUserMediaDeviceStorage(userMediaService.deviceStorage);
+    const micList = mediaDevices.filter((dev) => dev.kind === "audioinput");
+
+    const [selectedMic, setSelectedMic] = useState<string>(micList.at(NC.ZERO_IDX)?.deviceId ?? "");
+
+    const isMicDisabled = micStateInfo.state === MicState.DISABLED;
+    // TODO: disable button and select with this
+    const isBtnDisabled = (micStateInfo.state === MicState.LOADING || micList.length === NC.EMPTY_LENGTH);
+    const handleMicBtnClick = async (): Promise<void> =>
+    {
+        if (isMicDisabled)
+        {
+            await userMediaService.getMic(selectedMic);
+        }
+        else
+        {
+            userMediaService.stopMic();
+        }
+    };
+
     return (
         <List className="flex-auto">
             <p className="settings-category-label">{categoryName}</p>
             <p className="settings-section-label">Микрофон</p>
+            <p className="settings-group-label">Выбор микрофона</p>
+            <ListItemSelect
+                label={"Выбор микрофона"}
+                description={"Браузер не имеет права захватывать устройство микрофона без вашего разрешения. По возможности, выберите необходимое устройство и нажмите на кнопку захвата микрофона. После нажатия, браузер отобразит запрос на разрешение. Обратите внимание, что список устройств может быть неполным до того момента, пока вы не дадите разрешение."}
+                value={micStateInfo.id ?? selectedMic}
+                onValueChange={(val) =>
+                {
+                    setSelectedMic(val);
+                }}
+                options={micList.map((item) =>
+                {
+                    const option: ListItemSelectOption = { label: item.label, value: item.deviceId };
+                    return option;
+                })}
+            />
+            <ListItemButton
+                label={isMicDisabled ? "Микрофон не используется" : "В данный момент микрофон захвачен и используется"}
+                btnLabel={isMicDisabled ? "Захватить микрофон" : "Прекратить захват"}
+                onBtnClick={handleMicBtnClick}
+            />
             <p className="settings-group-label">Проверка звука</p>
             <div id="volume-meter-container">
                 <div id="volume-meter">
