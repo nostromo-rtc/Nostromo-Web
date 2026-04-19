b36cceb083c9459d048dbecea734c861b92886c7
SgAkErRu <sulmpx60@yandex.ru>
2026-04-19 18:44:39 +0400

    RoomWrapperPage: use `useAuth` hook
    Also get room name from `roomListModel`

--- src/pages/RoomWrapperPage.tsx
+++ src/pages/RoomWrapperPage.tsx
@@ -1,18 +1,54 @@
 import "../App.css";
 
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
+import { useParams, useSearchParams } from "react-router-dom";
 
+import { SocketManagerContext } from "../AppWrapper";
+import { useAuth } from "../hooks/UseAuth";
+import { useRoomListModel } from "../services/SocketService/RoomListModel";
 import { RoomAuthPage } from "./RoomAuthPage";
 import { RoomPage } from "./RoomPage";
 
 export const RoomWrapperPage: React.FC = () =>
 {
-    //const [auth, setAuth] = useState(false);
+    const { id } = useParams();
+    const [searchParams] = useSearchParams();
 
-    //TODO: убрать обратно на false
-    const [auth, setAuth] = useState(true);
+    const socketManager = useContext(SocketManagerContext);
+    const generalSocketService = socketManager.generalSocketService;
+    const roomList = useRoomListModel(generalSocketService.roomListModel);
+    const roomName = roomList.find((r) => r.id === id)?.name ?? "unknown";
 
-    return (
-        auth ? <RoomPage /> : <RoomAuthPage setAuth={setAuth} />
-    );
+    const path = (id != null) ? `/api/r/${id}` : "";
+    const password = searchParams.get("p");
+
+    const auth = useAuth(path, password);
+    const [ready, setReady] = useState(false);
+
+    const onSuccess = (): void =>
+    {
+        setReady(true);
+    };
+
+    if (id === undefined)
+    {
+        return <p>"404"</p>;
+    }
+
+    if (auth === "true" || ready)
+    {
+        return <RoomPage roomName={roomName} />;
+    }
+    else if (auth === "false")
+    {
+        return <RoomAuthPage roomName={roomName} onSuccess={onSuccess} />;
+    }
+    else
+    {
+        return (
+            <div id="main">
+                <span className="m-a">Ожидание ответа от сервера...</span>
+            </div>
+        );
+    }
 };
